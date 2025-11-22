fbfda010370dbfa7656af6234c1d013442e8d81c
tejas <tejas.shivanand.mane@gmail.com>
2025-11-21 21:31:00 -0800

    gcp script added

--- GCP_setup.sh
+++ GCP_setup.sh
@@ -1,27 +1,25 @@
-git clone https://github.com/tejas-shivanand-mane/stellar-core.git
+# --- Update and install basic tools ---
 sudo apt update
-sudo apt -y install git
-git clone https://github.com/tejas-shivanand-mane/stellar-core.git
-sudo apt -y install autoconf automake libtool
-sudo apt -y install build-essential
-sudo apt -y install autoconf automake libtool pkg-config build-essential libsodium-dev
-
-sudo apt -y install libpq-dev
-sudo apt -y install libunwind-dev
-
-sudo apt -y install flex bison
-
-sudo apt -y install cargo
+sudo apt -y upgrade
+sudo apt install -y git build-essential pkg-config autoconf automake libtool bison flex libpq-dev libunwind-dev parallel sed perl wget curl cmake
+# --- Install GCC 14 and G++ 14 and make default ---
+sudo apt install -y gcc-14 g++-14
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 100
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 100
+sudo update-alternatives --set gcc /usr/bin/gcc-14
+sudo update-alternatives --set g++ /usr/bin/g++-14
+# --- Install Clang 18 ---
+sudo apt install -y clang-18 libclang-18-dev libclang-rt-18-dev libunwind-18 libunwind-dev
+# --- Install Rust 1.88 using rustup ---
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source ~/.cargo/env
-rustup toolchain install 1.88.0
+source $HOME/.cargo/env
+rustup install 1.88.0
 rustup default 1.88.0
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-export PATH="$HOME/.cargo/bin:$PATH"
-
-
-cd stellar-core
-./autogen.sh
-./configure
-make -j$(nproc)
+# --- Verify Rust/Cargo ---
+rustc --version
+cargo --version
+# --- Install Stellar-Core dependencies ---
+sudo apt install -y libsodium-dev libpq-dev libunwind-dev
+git clone https://github.com/tejas-shivanand-mane/stellar-core.git
+cd stellar-core/
+./autogen.sh ; ./configure; make -j16
