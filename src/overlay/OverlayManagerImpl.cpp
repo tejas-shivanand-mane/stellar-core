d85ab3bad108dc7516dd67edef40a04e67152de2
tejas <tejas.shivanand.mane@gmail.com>
2025-12-18 14:44:26 -0800

    this works for 4 nodes

--- src/overlay/OverlayManagerImpl.cpp
+++ src/overlay/OverlayManagerImpl.cpp
@@ -2397,7 +2397,7 @@ OverlayManagerImpl::prop()
         
 
 
-        if (txn_count == 6000 && txn_count <= 6500) 
+        if (txn_count >= 6000 && txn_count <= 6500) 
         {
             CLOG_INFO(Overlay, "Forcing COLLECT round at txn_count={}", txn_count);
 
