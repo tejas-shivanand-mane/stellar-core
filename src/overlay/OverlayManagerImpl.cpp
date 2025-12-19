f7be305818f631c9a968cf6a52fc636caee0d6da
tejas <tejas.shivanand.mane@gmail.com>
2025-12-19 07:51:30 -0800

    this works for 4 nodes

--- src/overlay/OverlayManagerImpl.cpp
+++ src/overlay/OverlayManagerImpl.cpp
@@ -2397,7 +2397,7 @@ OverlayManagerImpl::prop()
         
 
 
-        if (currentView >= 4000 && currentView <= 4100) 
+        if (currentView >= 4000 && currentView <= 4050) 
         {
             CLOG_INFO(Overlay, "Forcing COLLECT round at txn_count={}", txn_count);
 
