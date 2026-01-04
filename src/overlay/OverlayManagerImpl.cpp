030d6dc8ecee6fc6ed68edb5e3a027361004af3f
tejas <tejas.shivanand.mane@gmail.com>
2026-01-03 17:12:09 -0800

    testing

--- src/overlay/OverlayManagerImpl.cpp
+++ src/overlay/OverlayManagerImpl.cpp
@@ -1884,7 +1884,7 @@ OverlayManagerImpl::prop()
         
 
 
-        if (currentView >= 70000 && currentView < 70001) 
+        if (currentView >= 20000 && currentView < 21000) 
         {
             CLOG_INFO(Overlay, "Forcing COLLECT round at txn_count={}", txn_count);
 
