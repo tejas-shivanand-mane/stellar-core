00d024e578528069de28b787620847a9e5d02496
tejas <tejas.shivanand.mane@gmail.com>
2026-01-10 16:18:09 -0800

    Fixed Failure Handling

--- src/overlay/OverlayManagerImpl.cpp
+++ src/overlay/OverlayManagerImpl.cpp
@@ -2667,10 +2667,10 @@ OverlayManagerImpl::recvCustomMessage(StellarMessage const& stellarMsg,
                       hexAbbrev(cm.blockHash), cm.view, computeNodeIndex());
 
 
-            // if (mApp.getConfig().MEMORY_PROF && cm.view > 30000 && cm.view%30000<20000 )
-            // {
-            //     return;
-            // }
+            if (mApp.getConfig().MEMORY_PROF && cm.view > 30000 && cm.view%10000<3000)
+            {
+                return;
+            }
             
 
             st.commitVoters.insert(sender);
@@ -2778,7 +2778,7 @@ OverlayManagerImpl::recvCustomMessage(StellarMessage const& stellarMsg,
 
 
 
-                // cleanupOldTxnStates();
+                cleanupOldTxnStates();
                 // st.executeVoters.insert(sender);
                 
             }
