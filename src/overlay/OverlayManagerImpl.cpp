7bece1598025ce8e9a44d53955d4da62219114e4
tejas <tejas.shivanand.mane@gmail.com>
2026-01-14 23:51:07 -0800

    checking failure

--- src/overlay/OverlayManagerImpl.cpp
+++ src/overlay/OverlayManagerImpl.cpp
@@ -2690,10 +2690,10 @@ OverlayManagerImpl::recvCustomMessage(StellarMessage const& stellarMsg,
                       hexAbbrev(cm.blockHash), cm.view, computeNodeIndex());
 
 
-            // if (mApp.getConfig().MEMORY_PROF && cm.view > 30000 && cm.view %10000 < 3000)
-            // {
-            //     return;
-            // }
+            if (mApp.getConfig().MEMORY_PROF && cm.view > 30000 && cm.view %10000 < 3000)
+            {
+                return;
+            }
             
 
             st.commitVoters.insert(sender);
