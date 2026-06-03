7cbc8ee162e4f99d1c39cd53fb47b06ae202a56f
tejas <tejas.shivanand.mane@gmail.com>
2026-06-02 18:47:14 -0700

    testing

--- src/overlay/OverlayManagerImpl.cpp
+++ src/overlay/OverlayManagerImpl.cpp
@@ -936,7 +936,7 @@ TransactionEnvelope createSCPTxFromProposal(
 // SCP TRACKING (Fair Comparison)
 // ============================================================================
 
-static bool ENABLE_SCP_TRACKING = true;
+static bool ENABLE_SCP_TRACKING = false;
 
 struct SCPStats {
     int totalBatches = 0;
@@ -1621,32 +1621,32 @@ OverlayManagerImpl::tick()
     // CLOG_INFO(Overlay, "This node's ID: {}", mApp.getConfig().toShortString(nodeID));
 
 
-    // if (pbft_start==0)
-    // {
+    if (pbft_start==0)
+    {
 
 
-    //     size_t authenticatedPeers = getAuthenticatedPeersCount();
-    //     size_t totalNodes = mApp.getConfig().KNOWN_PEERS.size(); // +1 for self
-    //     size_t expectedPeers = totalNodes - 1;
+        size_t authenticatedPeers = getAuthenticatedPeersCount();
+        size_t totalNodes = mApp.getConfig().KNOWN_PEERS.size(); // +1 for self
+        size_t expectedPeers = totalNodes - 1;
         
-    //     CLOG_INFO(Overlay, "authenticatedPeers,  expectedPeers: {}, {}", authenticatedPeers,  expectedPeers);
+        CLOG_INFO(Overlay, "authenticatedPeers,  expectedPeers: {}, {}", authenticatedPeers,  expectedPeers);
 
-    //     if (authenticatedPeers == expectedPeers)
+        if (authenticatedPeers == expectedPeers)
 
-    //     {
-    //         prop();
-    //         pbft_start = 1;
+        {
+            prop();
+            pbft_start = 1;
 
-    //         pbftStartTime = std::chrono::steady_clock::now();
-    //         pbftStartTimeSet = true;
+            pbftStartTime = std::chrono::steady_clock::now();
+            pbftStartTimeSet = true;
 
-    //         CLOG_INFO(Overlay, "PBFT started — timer armed");
-
-
+            CLOG_INFO(Overlay, "PBFT started — timer armed");
 
 
-    //     }
-    // }
+
+
+        }
+    }
 
 
     auto rescheduleTick = gsl::finally([&]() {
