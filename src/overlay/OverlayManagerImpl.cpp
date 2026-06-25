da4b90a30dd1b5cc4533ba7247b4eb35a8059c51
tejas <tejas.shivanand.mane@gmail.com>
2026-06-25 00:20:18 -0700

    testing

--- src/overlay/OverlayManagerImpl.cpp
+++ src/overlay/OverlayManagerImpl.cpp
@@ -3787,9 +3787,9 @@ OverlayManagerImpl::recvCustomMessage(StellarMessage const& stellarMsg,
 
 
                 // if (!(mApp.getConfig().MEMORY_PROF && cm.view > 20000 && cm.view %10000 < 3000))
-                {
-                    cleanupOldTxnStates();
-                }
+                // {
+                //     cleanupOldTxnStates();
+                // }
 
                 // Critical: deliver buffered messages for the new view before proposing again.
                 deliverBufferedForCurrentView();
