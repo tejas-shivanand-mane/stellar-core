1df67e8cb726a9133d32e0dd529a6fb6a7d56076
tejas <tejas.shivanand.mane@gmail.com>
2026-06-30 17:51:15 -0700

    set collect attempts 1

--- src/overlay/OverlayManagerImpl.cpp
+++ src/overlay/OverlayManagerImpl.cpp
@@ -107,10 +107,10 @@ static bool collectWindowActive = false;
 static uint64_t collectWindowStartView = 0;
 
 static uint64_t collectAttempts = 0;
-static constexpr uint64_t MAX_COLLECT_ATTEMPTS = 100;
+static constexpr uint64_t MAX_COLLECT_ATTEMPTS = 1;
 
 
-constexpr int FORCE_COLLECT_AFTER_SEC = 100;
+constexpr int FORCE_COLLECT_AFTER_SEC = 120;
 
 static bool collectWindowArmed = false;
 static uint64_t lastCollectSentView = UINT64_MAX;
